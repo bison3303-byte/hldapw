2d9eab83163e6822923a181d353c8a79ef90ec59
Hlda <Hildatulwardah@students.unnes.ac.id>
2022-11-24 13:11:42 +0700

    logindone

--- pages/sessionlogin.php
+++ pages/sessionlogin.php
@@ -1,27 +1,61 @@
 <?php 
+// mengaktifkan session pada php
 session_start();
-require "../functions/functions.php";
 
+// menghubungkan php dengan koneksi database
+include '../functions/functions.php';
+
+// menangkap data yang dikirim dari form login
 $username = $_POST['username'];
-$password = md5($_POST['password']);
-$result = mysqli_query($conn, "SELECT *FROM user WHERE username = '$username' AND password = '$password' ");
-$cek = mysqli_num_rows($result);
-      if($cek > 0){
-        $data=mysqli_fetch_array($result);
-          if($data['jabatan'] == "admin" ){
-            $_SESSION['username'] = $username;
-            $_SESSION['jabatan'] = "admin";
-            header("location:sidenavigationadmin.php");
-          }
-          else if ($data['jabatan'] == "kasir"){
-            $_SESSION['username'] = $username;
-            $_SESSION['jabatan'] = "kasir";
-            header("location:sidenavigationkasir.php");
-          } else {
-            header("location:login.php?pesan=gagal");
-          }
-        
-        }
+$password = $_POST['password'];
+
+
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($conn,"SELECT *FROM user where username='$username' and password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+
+	$data = mysqli_fetch_assoc($login);
+
+	// cek jika user login sebagai admin
+	if($data['level']=="admin"){
+
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "admin";
+		// alihkan ke halaman dashboard admin
+		header("location:sidenavigationadmin.php");
+
+	// cek jika user login sebagai pegawai
+	}else if($data['level']=="kasir"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "kasir";
+		// alihkan ke halaman dashboard pegawai
+		header("location:sidenavigationkasir.php");
+
+	// cek jika user login sebagai pengurus
+	}else if($data['level']=="pelanggan"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "pelanggan";
+		// alihkan ke halaman dashboard pengurus
+		header("location:sidenavigationpelanggan.php");
+
+	}else{
+
+		// alihkan ke halaman login kembali
+		header("location:index.php?pesan=gagal");
+	}
+
+	
+}else{
+	header("location:index.php?pesan=gagal");
+}
+
+
 
 ?>
-
