1fa582d6c0b7188c010365d1627d353349c1664e
ranuranu <rohmatn34@gmail.com>
2022-11-24 18:20:50 +0700

    datapesanan

--- pages/datapesanan.php
+++ pages/datapesanan.php
@@ -74,14 +74,14 @@ if ( isset($_POST["submit"])) {
             <span class="nav-link-text ms-1">Data Barang</span>
           </a>
         </li>
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link " href="Datapenjualan.php">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
             </div>
             <span class="nav-link-text ms-1">Data Penjualan</span>
           </a>
-        </li>
+        </li> -->
         <li class="nav-item">
           <a class="nav-link " href="datapesanan.php">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
