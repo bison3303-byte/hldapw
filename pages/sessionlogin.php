dc37cd9a8e7273f26e9c9e4719b4328db648b147
ranuranu <rohmatn34@gmail.com>
2022-11-24 17:56:36 +0700

    login update

--- pages/sessionlogin.php
+++ pages/sessionlogin.php
@@ -14,13 +14,8 @@ $query = mysqli_query($conn, "SELECT * FROM user WHERE
             $_SESSION['nama'] = $data ['nama'];
             $_SESSION['username']= $username;
             $_SESSION['level']=$data ['level'];
-?>
-Anda berhasil login. Silahkan menuju <a href="dashboard.php">HALAMAN HOME</a><br><br>
-<a href="session_logout.php">Log out</a>
-<?php 
-}else {
-?>
-Anda gagal login. Silahkan <a href="login.php">Login Kembali</a>
-<?php 
-}
+            header('Location: ../pages/dashboard.php');
+        }else {
+            header('Location: ../pages/login.php');
+        }
 ?>
