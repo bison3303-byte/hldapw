f3b4fe7c5ab07af92fdbd8b554262091b7ff9f6a
ranuranu <rohmatn34@gmail.com>
2022-11-25 12:56:18 +0700

    update kasir

--- pages/sidenavigationkasir.php
+++ pages/sidenavigationkasir.php
@@ -1,14 +1,14 @@
 <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main" style="overflow: unset;">
   <ul class="navbar-nav">
-    <li class="nav-item">
+    <!-- <li class="nav-item">
       <a class="nav-link active" href="dashboard.php">
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
         </div>
         <span class="nav-link-text ms-1">Dashboard</span>
       </a>
-    </li>
-    <li class="nav-item">
+    </li> -->
+    <!-- <li class="nav-item">
       <a class="nav-link" href="datapesanan.php">
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-calendar-grid-58 text-warning text-sm opacity-10"></i>
@@ -28,13 +28,13 @@
 
     <li class="nav-item mt-3">
       <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">KASIR</h6>
-    </li>
+    </li> -->
     <li class="nav-item">
       <a class="nav-link" href="datapesanan.php">
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Input Rekap Penjualan</span>
+        <span class="nav-link-text ms-1">Input Penjualan</span>
       </a>
     </li>
     <li class="nav-item">
@@ -42,7 +42,7 @@
         <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
           <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">Input Rekap Barang</span>
+        <span class="nav-link-text ms-1">Input Produk</span>
       </a>
     </li>
 
